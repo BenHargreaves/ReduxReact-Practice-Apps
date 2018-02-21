79c2b493ba1440162e2388454e487c75ba8191df
BenHargreaves <bhargreaves91@gmail.com>
2018-02-20 22:21:22 -0500

    Initial Commit (Booklist)

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        {title: 'Cracking the Coding Interview'},
+        {title: 'The Fireman'},
+        {title: 'Harry Potter'},
+        {title: 'Nos4r2'},
+        {title: 'Lightbringer'}
+    ]
+
+}
